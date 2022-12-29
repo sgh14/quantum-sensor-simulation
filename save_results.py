04f720ad53cec0516b83d8950098a978b285343d
Sergio García Heredia <sergio.gh14@gmail.com>
2022-12-29 13:01:54 +0100

    Fix data format of saved results

--- save_results.py
+++ save_results.py
@@ -4,12 +4,14 @@ import h5py
 
 
 def save_results(basis, probs, labels, file_path):
+    dirname = os.path.dirname(file_path)
     # Create a new HDF5 file
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    if dirname:
+        os.makedirs(dirname, exist_ok=True)
+        
     file = h5py.File(file_path, "w")
     # Create a dataset in the file
-    data_format = h5py.h5t.STD_U8BE
-    file.create_dataset("basis", np.shape(basis), data_format, basis)
-    file.create_dataset("probabilities", np.shape(probs), data_format, probs)
-    file.create_dataset("labels", np.shape(labels), data_format, labels)
+    file.create_dataset("basis", data=basis)
+    file.create_dataset("probabilities", data=probs)
+    file.create_dataset("labels", data=labels)
     file.close()
