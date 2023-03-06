f66cd82aaa986abcf08ca59c10c34e4c89484464
Sergio García Heredia <sergio.gh14@gmail.com>
2023-03-06 23:42:34 +0100

    Remove output_folder creation and check probabilities

--- save_results.py
+++ save_results.py
@@ -1,14 +1,11 @@
 import numpy as np
-import os
 import h5py
 
 
 def save_results(basis, probs, labels, file_path):
-    dirname = os.path.dirname(file_path)
-    # Create a new HDF5 file
-    if dirname:
-        os.makedirs(dirname, exist_ok=True)
-        
+    if np.any(np.abs(np.sum(probs, axis=1) - 1)) > 1e-10:
+        print('WARNING: probabilities do not add up to 1')
+
     file = h5py.File(file_path, "w")
     # Create a dataset in the file
     file.create_dataset("basis", data=basis)
