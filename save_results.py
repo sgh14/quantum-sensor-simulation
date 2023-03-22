4cd5767d61ebb8d4febd245019ee0db6542ad1be
Sergio García Heredia <sergio.gh14@gmail.com>
2023-03-22 23:40:26 +0100

    Fix probabilities check

--- save_results.py
+++ save_results.py
@@ -3,7 +3,7 @@ import h5py
 
 
 def save_results(basis, probs, labels, file_path):
-    if np.any(np.abs(np.sum(probs, axis=1) - 1)) > 1e-10:
+    if np.any(np.abs(np.sum(probs, axis=1) - 1) > 1e-10):
         print('WARNING: probabilities do not add up to 1')
 
     file = h5py.File(file_path, "w")
